3caa10d3b0a44681bba025ac131f8a9979a774c3
Piou-piou <pilloud.anthony@gmail.com>
2018-09-15 20:12:26 +0200

    [func] add about me page

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -10,8 +10,16 @@ class PagesController extends AbstractController
 	/**
 	 * @Route("/", name="index")
 	 */
-	public function indexAction()
+	public function index()
 	{
 		return $this->render("pages/index.html.twig");
+	}
+	
+	/**
+	 * @Route("/a-propos", name="a_propos")
+	 */
+	public function aPropos()
+	{
+		return $this->render("pages/a-propos.html.twig");
 	}
 }

52079ee2a3b0c2b3bdcbaedd98ae83bcc3adae42
Anthony Pilloud <pilloud.anthony@gmail.com>
2018-10-23 13:29:37 +0200

    [func] add ribs admin for portfolio edition

---
+++ src/Controller/PortfolioController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PortfolioController extends AbstractController {
+	
+	/**
+	 * @Route("/ribs-admin/portfolio/index/", name="ribsadmin_portfolio_index"))
+	 */
+	public function indexPortfolio() {
+	
+	}
+
+}

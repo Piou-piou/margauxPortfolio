187582ced0ae9fc60b69ad869c7136ac540eb869
Anthony Pilloud <pilloud.anthony@gmail.com>
2018-11-03 12:27:09 +0100

    [refa] indent of portfolio

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -2,25 +2,31 @@
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PortfolioController extends AbstractController {
+class PortfolioController extends AbstractController
+{
 	
 	/**
 	 * @Route("/ribs-admin/portfolio/index/", name="ribsadmin_portfolio_index"))
 	 */
-	public function index() {
+	public function index(): Response
+	{
 		return $this->render("admin/portfolio/index.html.twig", [
-			"articles" => []
+			"articles" => [],
 		]);
 	}
 	
 	/**
 	 * @Route("/ribs-admin/portfolio/create/", name="ribsadmin_portfolio_create"))
+	 * @param Request $request
+	 * @return \Symfony\Component\HttpFoundation\Response
 	 */
-	public function createProject() {
+	public function editProject(Request $request): Response
+	{
 		return $this->render("admin/portfolio/index.html.twig");
 	}
 }
